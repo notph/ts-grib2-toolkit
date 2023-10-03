b94dd0f309f3501cd40fd5e878521dc09b9004cb
meirborowski <borowskimeir@gmail.com>
2023-10-03 15:51:51 +0300

    Added an open method

--- src/core/grib2-file.ts
+++ src/core/grib2-file.ts
@@ -3,10 +3,16 @@ import * as fs from 'fs';
 class GRIB2File {
   private _filePath: string;
   private _fileData: Buffer = Buffer.alloc(0);
-  private _indicatorSection: any;
-  private _sections: any[] = [];
+  private _indicatorSection: unknown;
+  private _sections: unknown[] = [];
 
   constructor(filePath: string) {
     this._filePath = filePath;
   }
+
+  public async open(): Promise<void> {
+    this._fileData = await fs.promises.readFile(this._filePath);
+  }
 }
+
+export default GRIB2File;
